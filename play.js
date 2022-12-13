351166aded7631051b7b242cd9ef91362a3b5562
Jeremy Ho <jeremyho999@gmail.com>
2022-12-13 14:19:57 -0700

    Connect to the server Completed

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");   //interpret incoming data as text.
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
