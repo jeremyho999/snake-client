0de8bdb09d2e6dca969194d3cf54e112a4d55fcf
Jeremy Ho <jeremyho999@gmail.com>
2022-12-14 10:08:56 -0700

    Move commands Completed

--- client.js
+++ client.js
@@ -8,14 +8,25 @@ const connect = function() {
 
   conn.setEncoding("utf8");   //interpret incoming data as text.
 
-  conn.on("data", (data) => {               //receive data from server.
-    console.log("Server says: ", data);
+  conn.on("data", (dataFromServer) => {               //receive data from server.
+    console.log("Server says: ", dataFromServer);
   });
 
   conn.on("connect", () => {      //do something right after the first successful connection with Server:
     console.log("Successfully connected to game server!");    //print a message for the player (Client) to be notified
     conn.write("Name: JHO");      //send Server the name of the player (Client)
   });
+  
+  conn.on("connect", () => {
+    const intervalId = setInterval(() => {
+      conn.write("Move: up");
+    }, 50);
+
+    setTimeout(() => {
+      clearInterval(intervalId);
+    }, 500);
+  });
+
 
   return conn;
 };
