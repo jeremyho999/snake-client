b07fed62bf4f0f69b50856997d6f06f7c63bc146
Jeremy Ho <jeremyho999@gmail.com>
2022-12-14 10:52:49 -0700

    Stdin Setup completed

--- play.js
+++ play.js
@@ -3,3 +3,25 @@ const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
+
+// setup a function used by stdin event listener:
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin:
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // register an event listener for stdin, before you return the stdin obj:
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
